410c002f4de93fa6ebbe3f74cc4eb74aef3cf9a1
iMac <imac@iMacs-iMac.local>
2024-02-13 14:03:00 +0500

    add

--- src/models/test.js
+++ src/models/test.js
@@ -12,71 +12,86 @@ const testSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
-
     readingPara: {
       type: String,
       required: [true, "Reading paragraph is required"],
     },
 
     /*mcqs based test question*/
-    MultipleChoice: [
+    multipleChoice: [
       {
-        QuestionID: {
-          type: Number,
-        },
-        QuestionText: {
-          type: String,
-        },
-        CorrectAns: {
+        questionText: {
           type: String,
         },
 
-        QuestionOptions: [
+        choices: [
           {
             optionTitle: { type: String },
             optionValue: { type: String },
           },
         ],
+        correctChoiceIndex: {
+          type: Number,
+          required: true,
+          min: 0,
+          max: {
+            type: Number,
+            validate: [
+              validateChoicesLength,
+              "Correct choice index out of range",
+            ],
+          },
+        },
       },
     ],
 
-    /*Match The Headings the quesions*/
-    // MatchingHeading: [
-    //   {
-    //     QuestionID: {
-    //       type: Number,
-    //       default: 1,
-    //     },
-    //     QuestionText: {
-    //       type: String,
-    //     },
-    //     CorrectAns: {
-    //       type: String,
-    //     },
-
-    //     QuestionOptions: [
-    //       {
-    //         optionTitle: { type: String },
-    //         optionValue: { type: String },
-    //       },
-    //     ],
-    //   },
-    // ],
+    /*Match The information questions*/
+    matchingHeading: [
+      {
+        answerInfo: [{ answerOption: String }],
+        questionOptions: [
+          {
+            optionTitle: { type: String },
+            choices: [{ type: String }],
+            correctChoiceIndex: {
+              type: Number,
+              required: true,
+              min: 0,
+              max: {
+                type: Number,
+                validate: [
+                  validateChoicesLength,
+                  "Correct choice index out of range",
+                ],
+              },
+            },
+          },
+        ],
+      },
+    ],
 
     /*yes no and not given questions*/
-    TrueFalse: [
+    trueFalse: [
       {
-        QuestionID: {
-          type: Number,
-        },
-        QuestionText: {
-          type: String,
-        },
-        CorrectAns: {
+        questionText: {
           type: String,
         },
 
-        QuestionOptions: [
+        correctChoiceIndex: {
+          type: Number,
+          required: true,
+          min: 0,
+          max: {
+            type: Number,
+            validate: [
+              validateChoicesLength,
+              "Correct choice index out of range",
+            ],
+          },
+          enum:[0,1,2]
+        },
+
+        choices: [
           {
             optionValue: { type: String },
           },
@@ -91,36 +106,42 @@ const testSchema = new mongoose.Schema(
     ],
 
     /*Match The information questions*/
-    MatchingInformation: [
+    matchingInformation: [
       {
-        AnswerInfo: [
+        answerInfo: [
           {
-            answerOpt: String,
-            answerVal: String,
+            answerOption: String,
           },
         ],
-        QuestionOptions: [
+        questionOptions: [
           {
-            QuestionID: { type: Number },
             optionTitle: { type: String },
-            optionValue: { type: String },
-            correctAns: { type: String },
+
+            correctChoiceIndex: {
+              type: Number,
+              required: true,
+              min: 0,
+              max: {
+                type: Number,
+                validate: [
+                  validateChoicesLength,
+                  "Correct choice index out of range",
+                ],
+              },
+            },
+
+            choices: [{ type: String }],
           },
         ],
       },
     ],
-
-
-
-
-
   },
   { timestamps: true }
 );
 
+/*Validator function checking if that index exists in the array of options*/
+function validateChoicesLength(value) {
+  return this.choices && value < this.choices.length;
+}
+
 export default mongoose?.models?.tests || mongoose?.model("tests", testSchema);
-
-
-
-
-
