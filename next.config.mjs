3101712651db9ac7ea08d92776f63ac9c9dc506c
iMac <imac@iMacs-iMac.local>
2024-03-13 16:17:43 +0500

    cors api routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-
   reactStrictMode: true,
   images: {
     remotePatterns: [
@@ -27,12 +25,32 @@ const nextConfig = {
       },
     ],
   },
-
-
   env: {
     MONGO_URI:
       "mongodb+srv://test:123@cluster0.ya051ny.mongodb.net/test?retryWrites=true&w=majority",
-      SECURE_URL: "CODEWITHEDIFY",
+    SECURE_URL: "CODEWITHEDIFY",
+  },
+
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
   },
 };
 
